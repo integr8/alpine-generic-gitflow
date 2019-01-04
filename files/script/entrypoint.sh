8a95bea1be8cae652465fbc649eda719f77be8c7
Fábio Luciano <fabio@naoimporta.com>
2019-01-04 17:04:43 -0200

    Atualizando estrutura

--- files/script/entrypoint.sh
+++ files/script/entrypoint.sh
@@ -5,6 +5,17 @@ export SOURCE_PATH='/opt/source'
 export BINARY_PATH=$(dirname "$0")
 
 source $BINARY_PATH/source.sh
+source $BINARY_PATH/bump.sh
 
+printf "\n\n" | git flow init
 
-git flow init
+case $1 in
+  feature) echo feature ;;
+  bugfix) echo bugfix ;;
+  release)
+    source $BINARY_PATH/wrapper/release.sh
+  ;;
+  hotfix) echo hotfix ;;
+  support) echo support ;;
+  *) echo 'sho me how it works' ;;
+esac
